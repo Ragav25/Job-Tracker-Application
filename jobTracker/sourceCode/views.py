450266046b9643dc5339389440ab4adf7230b679
Vinitha Raghavendar <viniraga@vinithas-mbp.home>
2020-06-10 03:58:34 +0100

    initial commit

---
+++ jobTracker/sourceCode/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+
+appliedJobs =[
+    {
+        'role': 'Software Developer',
+        'company': 'Precision IT Systems',
+        'status': 'Pending',
+        'date_applied': 'June 20'
+    },
+    {
+        'role': 'Data Analytics',
+        'company': 'Saber',
+        'status': 'Pending',
+        'date_applied': 'June 21'
+    }
+]
+
+def home(request):
+    context = {
+        'appliedJobs' : appliedJobs
+    }
+    return render(request, 'sourceCode/home.html', context)
+
+
+def about(request):
+    return render(request, 'sourceCode/about.html', {'title': 'About'})
